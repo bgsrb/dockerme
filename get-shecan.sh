3f62600aa3e8fc346e60d945903ac445ac23565d
bagher sohrabi <bgsrb88@gmail.com>
2020-03-12 20:33:35 +0330

    Update get-shecan.sh

--- get-shecan.sh
+++ get-shecan.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cat /etc/resolv.conf > resolv.conf.old
-echo "nameserver 178.22.122.100\nnameserver 185.51.200.2" > /etc/resolv.conf
+sudo sh -c "echo -n "" > /etc/resolv.conf"
+sudo sh -c "echo nameserver 178.22.122.100 >> /etc/resolv.conf"
+sudo sh -c "echo nameserver 185.51.200.2 >> /etc/resolv.conf"
