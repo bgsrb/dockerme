c7b09d6f4b5f36c13e0a8d39f2653cd0d8a17b57
bagher sohrabi <bgsrb88@gmail.com>
2020-03-12 19:56:49 +0330

    Create get-docker-compose.sh

---
+++ get-docker-compose.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | sort --version-sort | tail -n 1`
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
