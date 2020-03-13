7ed19e2fa795b76194d70952b6aa2dd3b6275575
bagher sohrabi <bgsrb88@gmail.com>
2020-03-13 18:31:27 +0330

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# install shecan
+curl -fsSL https://bgsrb.github.io/dockerme/get-shecan.sh -o get-shecan.sh
+sudo sh get-shecan.sh
+
+# install docker
+curl -fsSL https://bgsrb.github.io/dockerme/get-docker.sh -o get-docker.sh
+sudo sh get-docker.sh 
